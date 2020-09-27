5f56e5f466691b2a0913a0bfa7da02c202980711
krishna716 <krishb2000@hotmail.com>
2020-09-27 15:09:19 -0500

    modified text to trigger auto build

--- src/MyMicroservice/Startup.cs
+++ src/MyMicroservice/Startup.cs
@@ -32,7 +32,7 @@ namespace MyMicroservice
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello World my first microservice program!");
                 });
             });
         }
